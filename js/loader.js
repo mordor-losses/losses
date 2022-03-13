6d02bea6dd6b8550dbb6989df4779815770151db
Roman Olesiv <mordor.losses@gmail.com>
2022-03-13 12:57:26 +0200

    const instead of let

--- js/loader.js
+++ js/loader.js
@@ -4,7 +4,7 @@
     const date = new Date();
     const timeCache = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), 0, 0).getTime();
     files.forEach(function(file) {
-        let script = document.createElement('script');
+        const script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'js/' + file + '.js?' + timeCache ;
 
