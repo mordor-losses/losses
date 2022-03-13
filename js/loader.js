43a0f90d309b4e2dcb66a7fa362f1ef5924803b8
Roman Olesiv <mordor.losses@gmail.com>
2022-03-13 12:52:09 +0200

    add boost time move files to folders

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+(function() {
+    document.getElementsByName('head')
+    const files = ['lang', 'data', 'main'];
+    const date = new Date();
+    const timeCache = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), 0, 0).getTime();
+    files.forEach(function(file) {
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'js/' + file + '.js?' + timeCache ;
+
+        document.head.appendChild(script);
+
+    })
+})();
