ed8c77b0e3c46802c332bfcc65f9ce43db31b727
Roman Olesiv <mordor.losses@gmail.com>
2022-03-14 12:55:48 +0200

    move html files inside of folder

--- js/loader.js
+++ js/loader.js
@@ -1,15 +1,14 @@
 (function() {
-    const languageMatch = window.location.href.match(/index_(.+).html/);
-    const language = !languageMatch ? 'ua' : languageMatch[1];
+    const language = window.location.href.match(/index_(.+).html/);
 
     document.getElementsByName('head')
-    const files = ['languages/' + language, 'data', 'main'];
+    const files = ['languages/' + language[1], 'data', 'main'];
     const date = new Date();
     const timeCache = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), 0, 0).getTime();
     files.forEach(function(file) {
         const script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'js/' + file + '.js?t=' + timeCache ;
+        script.src = '../js/' + file + '.js?t=' + timeCache ;
         script.async = false;
 
         document.head.appendChild(script);
