d40bf5c187f48a18dc8ac93b66ef5cc2fedec38b
Roman Olesiv <mordor.losses@gmail.com>
2022-04-01 12:07:53 +0300

    01.04

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
         }
 
         const startDate = new Date('2022-02-24');
-        const endDate = new Date('2022-03-31');
+        const endDate = new Date('2022-04-01');
         let currentDate = startDate;
         const labels = [];
 
