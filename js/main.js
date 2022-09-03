57c24cfd7b3e65245a33b563de92aea4288aebde
Roman Olesiv <mordor.losses@gmail.com>
2022-09-03 22:39:16 +0300

    Added filter for dates (only for UA)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,64 @@
 (function() {
+    const START_OF_WAR = '2022-02-24';
+    const END_OF_WAR = "2022-09-03";
+    let charts = [];
     let currentIconElement = null;
     let fullScreen = false;
     function init() {
         renderCharts(window.languageData.charts);
         renderFullScreenIcons();
         addEvents();
+        initDatePicker();
+    }
+
+    function calculateCountOfDays(firstDate, secondDate) {
+        const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+
+        return Math.round(Math.abs((new Date(firstDate) - new Date(secondDate)) / oneDay));
+    }
+
+    function initDatePicker() {
+        const picker = new easepick.create({
+            element: "#datepicker",
+            css: [
+                "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.0/dist/index.css"
+            ],
+            zIndex: 10,
+            plugins: [
+                "RangePlugin",
+                "LockPlugin",
+                "PresetPlugin"
+            ],
+            "LockPlugin": {
+                "minDate": START_OF_WAR,
+                "maxDate": END_OF_WAR,
+            }
+        });
+
+        picker.on('select', (e) => {
+            const startDate = new Date(e.detail.start);
+            const endDate = new Date(e.detail.end);
+
+            applyDateFilters(startDate, endDate);
+        })
+    }
+
+    function applyDateFilters(startDate, endDate) {
+        const countDays = calculateCountOfDays(endDate, startDate);
+        const countDays2 = calculateCountOfDays(startDate, START_OF_WAR);
+
+        charts.forEach((chart) => {
+            if (!chart.config.dataBackup) {
+                chart.config.dataBackup = JSON.parse(JSON.stringify(chart.config.data));
+            }
+
+            chart.config.dataBackup.datasets.forEach((dataset, index) => {
+                chart.config.data.datasets[index].data = dataset.data.slice(countDays2, countDays2 + countDays + 1);
+            });
+
+            chart.config.data.labels = chart.config.dataBackup.labels.slice(countDays2, countDays2 + countDays + 1);
+            chart.update();
+        });
     }
 
     function renderFullScreenIcons() {
@@ -15,7 +69,27 @@
         })
     }
 
-    function getDateLabels(countDays) {
+    function getDateLabels(fromDate, toDate) {
+        const formatMonth = function (month) {
+            const formattedMonth = month + 1;
+
+            return formattedMonth >= 10 ? formattedMonth : '0' + formattedMonth
+        }
+
+        let currentDate = new Date(fromDate);
+        let endDate = new Date(toDate);
+
+        const labels = [];
+        while (currentDate.getTime() <= endDate.getTime()) {
+            labels.push(currentDate.getDate() + '.' + formatMonth(currentDate.getMonth()));
+            currentDate = new Date(currentDate.getTime() + 86400000)
+        }
+
+        return labels;
+    }
+
+    /** @deprecated **/
+    function getOldDateLabels(countDays) {
         const formatMonth = function (month) {
             const formattedMonth = month + 1;
 
@@ -34,11 +108,11 @@
     }
 
     function renderChart(idElement, datasets, chartTitle) {
-        const labels = getDateLabels(datasets[0].data.length);
+        const labels = getDateLabels(START_OF_WAR, END_OF_WAR);
         return new Chart(idElement, {
             data: {
-                datasets: datasets,
-                labels: labels
+                datasets,
+                labels
             },
             options: {
                 maintainAspectRatio: false,
@@ -183,10 +257,13 @@
         const earthOtherLosses = [ppos, gradAndBucks, boats, special];
         const personalLosses = [personal];
 
-        renderChart('chartMordorLossesEarth', earthLosses, languageData.title.earth);
-        renderChart('chartMordorLossesSky', skyLosses, languageData.title.sky);
-        renderChart('chartMordorLossesEarthOther', earthOtherLosses, languageData.title.earthOther);
-        renderChart('chartMordorLossesPersonal', personalLosses, languageData.title.personal);
+
+        charts.push(
+            renderChart('chartMordorLossesEarth', earthLosses, languageData.title.earth),
+            renderChart('chartMordorLossesSky', skyLosses, languageData.title.sky),
+            renderChart('chartMordorLossesEarthOther', earthOtherLosses, languageData.title.earthOther),
+            renderChart('chartMordorLossesPersonal', personalLosses, languageData.title.personal),
+        )
     }
 
     function toggleFullScreenMode(element) {
