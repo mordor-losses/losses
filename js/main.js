06e3647f04424db348b3d3180b969ccc54c0d387
Roman Olesiv <mordor.losses@gmail.com>
2024-06-09 10:58:28 +0300

    07.06

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
     const START_OF_WAR = '2022-02-24';
-    const END_OF_WAR = '2024-06-06';
+    const END_OF_WAR = '2024-06-07';
     let charts = [];
     let currentIconElement = null;
     let fullScreen = false;
